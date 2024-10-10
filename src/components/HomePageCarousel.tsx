f15cb387c341fa91dbda5bf22b9c109f5d899255
RishiAP <debjyotim2019@gmail.com>
2024-10-10 11:44:17 +0530

    Added pics

--- src/components/HomePageCarousel.tsx
+++ src/components/HomePageCarousel.tsx
@@ -2,16 +2,16 @@
 "use client";
 
 import { Carousel } from "flowbite-react";
+import Image from "next/image";
 
 export default function HomePageCarousel() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 max-w-7xl m-auto">
       <Carousel slideInterval={5000}>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <Image width={1280} height={784} style={{width:"100%"}} src="https://res.cloudinary.com/dnxfq38fr/image/upload/v1728536795/q62iuicnjzxlvkysldxa.jpg" alt="..." />
+        <Image width={1280} height={784} style={{width:"100%"}} src="https://res.cloudinary.com/dnxfq38fr/image/upload/v1728537925/rfjd8dqlj7miytebyvwl.jpg" alt="..." />
+        <Image width={1280} height={784} style={{width:"100%"}} src="https://res.cloudinary.com/dnxfq38fr/image/upload/v1728538383/zjyoia6wmebknkscwelt.jpg" alt="..." />
+        <Image width={1280} height={784} style={{width:"100%"}} src="https://res.cloudinary.com/dnxfq38fr/image/upload/v1728538557/veoysddckoaikjgrqqj1.jpg" alt="..." />
       </Carousel>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,24 +2,40 @@
 "use client";
 
 import Link from "next/link";
-import { Button, Navbar } from "flowbite-react";
+import { Button, Navbar, Spinner } from "flowbite-react";
+import { useEffect, useState } from "react";
 
 export function AppBar() {
+  const [loading,setLoading]=useState(false);
+  const [activePath,setActivePath]=useState('');
+  function handleClickLoader(e:any){
+    if(e.target.tagName==="A" && window.location.pathname!==e.target.pathname || e.target.tagName==="BUTTON" && e.target.children[0].tagName==="A" && window.location.pathname!==e.target.children[0].pathname)
+    setLoading(true);
+  }
+  useEffect(() => {
+    setActivePath(window.location.pathname);
+  },[]);
+
   return (
+    <>
     <Navbar fluid rounded>
       <Navbar.Brand href="/">
         <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Alumni Cell Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Alumni Cell</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button> <Link href="/be-a-member">Be a member</Link> </Button>
+        <Button onClick={handleClickLoader}> <Link href="/be-a-member">Be a member</Link> </Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
+        <Navbar.Link as={Link} href="/" onClick={handleClickLoader} active={activePath=="/"}>
           Home
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
+    {
+      loading && <div className="absolute flex w-full h-full top-0 z-10 justify-center items-center" style={{background:"#cdcdcd59"}}><Spinner aria-label="Extra large spinner example" size="xl" color={"purple"} /></div>
+    }
+    </>
   );
 }
